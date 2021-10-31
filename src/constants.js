3a60129a39dc9ad53d0352ceb5e43e553a4676a6
Greg Plumbly <greg.plumbly@gmail.com>
2021-10-31 17:10:09 +0000

    first commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x09aD4A89ef7965A0DD0E519f329A448f8Ce1D1c0";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
